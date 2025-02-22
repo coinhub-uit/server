eb61f78ace39b1fbc1d8b10c97ee80830bdc35e9
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-22 21:17:22 +0700

    chore(dev): cleanup

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,22 +2,6 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'ci',
-      ],
-    ],
-  },
 };
-module.exports = Configuration;
+
+export default Configuration;
