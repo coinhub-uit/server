ea14126b51a02da656442804f700216a08674f1f
NTGNguyen <ntgnguyen@duck.com>
2025-02-26 11:57:16 +0700

    feat: set up database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Method } from 'src/modules/users/entities/method.entity';
+import { Notification } from 'src/modules/users/entities/notification.entity';
+import { Rate } from 'src/modules/users/entities/rate.entity';
+import { Ticket } from 'src/modules/users/entities/ticket.entity';
+import { Transaction } from 'src/modules/users/entities/transaction.entity';
+import { User } from 'src/modules/users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Method, Notification, Rate, Ticket, Transaction, User],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from 'src/modules/users/controllers/users.controller';
 import { Method } from 'src/modules/users/entities/method.entity';
+import { Notification } from 'src/modules/users/entities/notification.entity';
+import { Rate } from 'src/modules/users/entities/rate.entity';
 import { Ticket } from 'src/modules/users/entities/ticket.entity';
+import { Transaction } from 'src/modules/users/entities/transaction.entity';
 import { User } from 'src/modules/users/entities/user.entity';
 import { UsersService } from 'src/modules/users/services/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Ticket, Method])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Method,
+      Ticket,
+      User,
+      Rate,
+      Notification,
+      Transaction,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UsersService],
 })
