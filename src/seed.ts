69a41453e3076af4ccd2192ff11762a8baf261ef
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-03-13 23:30:37 +0700

    fix: done seeding
    Im coollll

--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,10 @@
-import { seedDataSourceOptions } from 'src/common/database/seed-options';
+import { datasourceOptions } from 'src/common/database/options';
+import { resolve as pathResolve } from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { runSeeders } from 'typeorm-extension';
 
 async function seed() {
-  const options: DataSourceOptions = seedDataSourceOptions;
+  const options: DataSourceOptions = datasourceOptions;
 
   // NOTE: We don't need to create db here. Just seeding
 
@@ -12,6 +14,10 @@ async function seed() {
 
   const dataSource = new DataSource(options);
   await dataSource.initialize();
+
+  await runSeeders(dataSource, {
+    seeds: [pathResolve(__dirname, '..') + '/**/seeds/*.seeder.{ts,js}'],
+  });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
