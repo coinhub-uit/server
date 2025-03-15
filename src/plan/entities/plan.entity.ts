463e884dd24cfdfc838a5b67bd00e12268a8cabd
NTGNguyen <ntgnguyen@duck.com>
2025-03-15 13:54:21 +0700

    chore: trigger refresh available plan on event

--- src/plan/entities/plan.entity.ts
+++ src/plan/entities/plan.entity.ts
@@ -1,6 +1,15 @@
 import { PlanHistoryEntity } from 'src/plan/entities/plan_history.entity';
 import { AbstractEntity } from 'src/utils/abstract.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  EntityManager,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('plan')
 export class PlanEntity extends AbstractEntity<PlanEntity> {
@@ -15,4 +24,11 @@ export class PlanEntity extends AbstractEntity<PlanEntity> {
 
   @OneToMany(() => PlanHistoryEntity, (planHistory) => planHistory.plan)
   planHistories: PlanHistoryEntity[];
+
+  @AfterInsert()
+  @AfterUpdate()
+  @AfterRemove()
+  async refreshAvailablePlan(manager: EntityManager) {
+    await manager.query('REFRESH MATERIALIZED VIEW available_plan;');
+  }
 }
