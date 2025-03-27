bd4fca516e2cfb863665362596353399d517dcdb
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-03-27 08:00:27 +0700

    chore: cleanup merged redundant files

--- src/common/utils/generate_txn_ref.ts
+++
@@ -1,8 +0,0 @@
-export function generateTxnRef(): string {
-  const timestamp = Date.now();
-  const randomString = Array.from({ length: 6 }, () =>
-    Math.random().toString(36).charAt(2).toUpperCase(),
-  ).join('');
-  const txnRef = `VN${timestamp}${randomString}`;
-  return txnRef;
-}
