615a6f5dbf1a219a20a831324d2b7926c9974787
NTGNguyen <ntgnguyen@duck.com>
2025-05-08 13:34:06 +0700

    chore(ai-chat): use user repository, not user service

--- src/ai-chat/services/ai-chat.service.ts
+++ src/ai-chat/services/ai-chat.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import OpenAI from 'openai';
 import { AiChatRequestDto } from 'src/ai-chat/dtos/ai-chat.request.dto';
 import { AiChatResponseDto } from 'src/ai-chat/dtos/ai-chat.response.dto';
 import { AiChatSession } from 'src/ai-chat/types/ai-chat-session.type';
 import { UserJwtRequest } from 'src/auth/types/user.jwt-request';
-import { UserService } from 'src/user/services/user.service';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UserNotExistException } from 'src/user/exceptions/user-not-exist.exception';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AiChatService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   private readonly openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -17,6 +23,29 @@ export class AiChatService {
 
   private OPENAI_MODEL = process.env.OPENAI_MODEL;
 
+  private async getUserInformation(userId: string) {
+    const user = await this.userRepository.findOneOrFail({
+      where: { id: userId },
+      relations: {
+        sources: {
+          topUps: true,
+          tickets: {
+            ticketHistories: {
+              planHistory: {
+                plan: true,
+              },
+            },
+          },
+        },
+        notifications: true,
+      },
+    });
+    if (!user) {
+      throw new UserNotExistException();
+    }
+    return user;
+  }
+
   async ask({
     aiChatSession,
     aiChatRequestDto,
@@ -24,9 +53,7 @@ export class AiChatService {
     aiChatSession: AiChatSession;
     aiChatRequestDto: AiChatRequestDto & { user: UserJwtRequest };
   }) {
-    const current_user = this.userService.findAllUserInformation(
-      aiChatRequestDto.user.userId,
-    );
+    const current_user = this.getUserInformation(aiChatRequestDto.user.userId);
     if (aiChatSession.messages) {
       aiChatSession.messages.push({
         role: 'user',
