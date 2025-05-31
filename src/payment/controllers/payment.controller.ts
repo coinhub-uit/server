27cda3f7e2f565be2211baae9815eda1a286e72e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-05-31 20:06:49 +0700

    fix(payment): semantic and docs for topup endpoint

--- src/payment/controllers/payment.controller.ts
+++ src/payment/controllers/payment.controller.ts
@@ -10,9 +10,9 @@ import {
 } from '@nestjs/common';
 import {
   ApiBearerAuth,
+  ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiNotImplementedResponse,
-  ApiOkResponse,
   ApiOperation,
 } from '@nestjs/swagger';
 import { Request } from 'express';
@@ -66,11 +66,12 @@ export class PaymentController {
     summary: 'Generate top up link',
     description: 'Generate top up link',
   })
-  @ApiOkResponse({
+  @ApiCreatedResponse({
     description: 'VNPAY top up link',
     example: {
       url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?vnp_Amount=1806000&vnp_Command=pay&vnp_CreateDate=20210801153333&vnp_CurrCode=VND&vnp_IpAddr=127.0.0.1&vnp_Locale=vn&vnp_OrderInfo=Thanh+toan+don+hang+%3A5&vnp_OrderType=other&vnp_ReturnUrl=https%3A%2F%2Fdomainmerchant.vn%2FReturnUrl&vnp_TmnCode=DEMOV210&vnp_TxnRef=5&vnp_Version=2.1.0&vnp_SecureHash=3e0d61a0c0534b2e36680b3f7277743e8784cc4e1d68fa7d276e79c23be7d6318d338b477910a27992f5057bb1582bd44bd82ae8009ffaf6d141219218625c42',
     } satisfies CreateTopUpResponseDto,
+    type: CreateTopUpResponseDto,
   })
   @ApiNotImplementedResponse()
   @UseGuards(UserJwtAuthGuard)
