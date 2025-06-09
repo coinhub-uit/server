52605b4db856d7c8ac1dfcbf0b3a26754748f3f3
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-09 21:52:40 +0700

    chore(payment): no import user module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -7,14 +7,12 @@ import vnpayConfig from 'src/config/vnpay.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TopUpEntity } from 'src/payment/entities/top-up.entity';
 import { SourceModule } from 'src/source/source.module';
-import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     VnpayModule.registerAsync(vnpayConfig.asProvider()),
     TypeOrmModule.forFeature([TopUpEntity]),
     SourceModule,
-    UserModule,
   ],
   controllers: [PaymentController],
   providers: [PaymentService, VnpayService],
