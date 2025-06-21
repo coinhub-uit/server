67780882f8a0992e73b436cd129d30e2a8624e66
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-21 20:54:28 +0700

    fix(user): return deleted user also for get all users

--- src/user/configs/user-pagination.config.ts
+++ src/user/configs/user-pagination.config.ts
@@ -2,6 +2,7 @@ import { PaginateConfig, PaginationType } from 'nestjs-paginate';
 import { UserEntity } from 'src/user/entities/user.entity';
 
 export const userPaginationConfig: PaginateConfig<UserEntity> = {
+  withDeleted: true,
   sortableColumns: ['createdAt'],
   paginationType: PaginationType.CURSOR,
 };
